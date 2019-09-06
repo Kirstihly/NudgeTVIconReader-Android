a54f6dc741359caea2653333912858321a9c9a2e
Kirstihly <kirstinewcentury@gmail.com>
2019-09-06 15:18:58 -0700

    first pull

---
+++ app/src/main/java/leying/nudgetviconreader/MainActivity.java
@@ -0,0 +1,37 @@
+package leying.nudgetviconreader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnScanIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        btnScanIcon = findViewById(R.id.btnScanIcon);
+        btnScanIcon.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btnScanIcon:
+                startActivity(new Intent(MainActivity.this, ScannedCodeActivity.class));
+                break;
+        }
+
+    }
+}
